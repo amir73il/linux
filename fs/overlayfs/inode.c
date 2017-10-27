f958bbbf3d4889fa01867208041117a0b3f1af9e
Amir Goldstein <amir73il@gmail.com>
2017-10-27 15:44:08 +0300

    ovl: lockdep annotate of nested OVL_I(inode)->lock
    This fixes a lockdep splat when mounting a nested overlayfs.
    
    Fixes: a015dafcaf5b ("ovl: use ovl_inode mutex to synchronize...")
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/inode.c
+++ fs/overlayfs/inode.c
@@ -410,6 +410,7 @@ static inline void ovl_lockdep_annotate_inode_mutex_key(struct inode *inode)
 #ifdef CONFIG_LOCKDEP
 	static struct lock_class_key ovl_i_mutex_key[OVL_MAX_NESTING];
 	static struct lock_class_key ovl_i_mutex_dir_key[OVL_MAX_NESTING];
+	static struct lock_class_key ovl_i_lock_key[OVL_MAX_NESTING];
 
 	int depth = inode->i_sb->s_stack_depth - 1;
 
@@ -420,6 +421,8 @@ static inline void ovl_lockdep_annotate_inode_mutex_key(struct inode *inode)
 		lockdep_set_class(&inode->i_rwsem, &ovl_i_mutex_dir_key[depth]);
 	else
 		lockdep_set_class(&inode->i_rwsem, &ovl_i_mutex_key[depth]);
+
+	lockdep_set_class(&OVL_I(inode)->lock, &ovl_i_lock_key[depth]);
 #endif
 }
 
