de2732c8a3fd66caf3494002566ff2f6104867f9
Amir Goldstein <amir73il@gmail.com>
2018-11-12 22:28:17 +0200

    ovl: remount snapshot fs with new snapshot overlay
    Allow changing the snapshot overlay of snapshot fs by provding
    "snapshot=<snappath>" or "nosnapshot" argument to remount.
    
    ovl_snapshot_remount() changes the value of ofs->snap->mnt
    to the new snapshot overlay and increments ofs->snap->id
    to invalidate dentry snapflags that are relevant for the
    old snapshot id.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -218,6 +218,23 @@ void ovl_destroy_inode(struct inode *inode)
 	call_rcu(&inode->i_rcu, ovl_i_callback);
 }
 
+void ovl_free_config(struct ovl_config *config)
+{
+	kfree(config->snapshot);
+	kfree(config->lowerdir);
+	kfree(config->upperdir);
+	kfree(config->workdir);
+	kfree(config->redirect_mode);
+}
+
+void ovl_snap_free(struct ovl_snap *snap)
+{
+	if (snap) {
+		mntput(snap->mnt);
+		kfree(snap);
+	}
+}
+
 void ovl_free_fs(struct ovl_fs *ofs)
 {
 	unsigned i;
@@ -234,10 +251,7 @@ void ovl_free_fs(struct ovl_fs *ofs)
 	if (ofs->upperdir_locked)
 		ovl_inuse_unlock(ofs->upper_mnt->mnt_root);
 	mntput(ofs->upper_mnt);
-	if (ofs->snap) {
-		mntput(ofs->snap->mnt);
-		kfree(ofs->snap);
-	}
+	ovl_snap_free(ofs->snap);
 	for (i = 0; i < ofs->numlower; i++) {
 		iput(ofs->lower_layers[i].trap);
 		mntput(ofs->lower_layers[i].mnt);
@@ -247,11 +261,7 @@ void ovl_free_fs(struct ovl_fs *ofs)
 	kfree(ofs->lower_layers);
 	kfree(ofs->lower_fs);
 
-	kfree(ofs->config.snapshot);
-	kfree(ofs->config.lowerdir);
-	kfree(ofs->config.upperdir);
-	kfree(ofs->config.workdir);
-	kfree(ofs->config.redirect_mode);
+	ovl_free_config(&ofs->config);
 	if (ofs->creator_cred)
 		put_cred(ofs->creator_cred);
 	kfree(ofs);
