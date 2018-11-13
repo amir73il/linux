41cb58d4a0922d73a70e9d17a07f1ab1483fe0a8
Amir Goldstein <amir73il@gmail.com>
2018-11-13 18:34:02 +0200

    ovl: activate new snapshot on remount ro/rw cycle
    To support "atomic" snapshot take from application persperctive,
    keep new configured snapshot pending until the next ro remount.
    
    New snapshot becomes effective immediately when remounting readonly,
    so this is an example for taking a new snapshot:
     mount -o remount,ro,snapshot=<newsnmap> /mnt
     mount -o remount,rw /mnt
    
    Note that remount ro will fail if there are files open for write.
    Overlayfs snapshots do not support snapshotting files open for
    write yet.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -248,6 +248,7 @@ void ovl_free_fs(struct ovl_fs *ofs)
 		ovl_inuse_unlock(ofs->upper_mnt->mnt_root);
 	mntput(ofs->upper_mnt);
 	ovl_snap_free(ofs->snap);
+	ovl_snap_free(ofs->new_snap);
 	for (i = 0; i < ofs->numlower; i++)
 		mntput(ofs->lower_layers[i].mnt);
 	for (i = 0; i < ofs->numlowerfs; i++)
