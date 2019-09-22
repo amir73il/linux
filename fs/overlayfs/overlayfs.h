e816fe58b370342940632c440013a725c21124bf
Amir Goldstein <amir73il@gmail.com>
2019-09-22 18:58:23 +0300

    ovl: whiteout only directories in snapshot with metacopy=on
    Before a directory is renamed, we usually cover the target with a
    whiteout. When directory is exchanged with a file in a metacopy
    snapshot, we cover the target with an empty file instead.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/overlayfs.h
+++ fs/overlayfs/overlayfs.h
@@ -558,7 +558,7 @@ extern const struct inode_operations ovl_snapshot_inode_operations;
 int ovl_snapshot_fs_register(void);
 void ovl_snapshot_fs_unregister(void);
 int ovl_snapshot_open(struct dentry *dentry, unsigned int flags);
-int ovl_snapshot_modify(struct dentry *dentry);
+int ovl_snapshot_modify(struct dentry *dentry, bool new_is_dir);
 
 static inline bool ovl_is_snapshot_fs_type(struct super_block *sb)
 {
@@ -574,12 +574,13 @@ static inline int ovl_snapshot_maybe_copy_up(struct dentry *dentry,
 	return ovl_snapshot_open(dentry, flags);
 }
 
-static inline int ovl_snapshot_want_write(struct dentry *dentry)
+static inline int ovl_snapshot_want_write(struct dentry *dentry,
+					  bool new_is_dir)
 {
 	if (!ovl_is_snapshot_fs_type(dentry->d_sb))
 		return 0;
 
-	return ovl_snapshot_modify(dentry);
+	return ovl_snapshot_modify(dentry, new_is_dir);
 }
 
 static inline void ovl_snapshot_drop_write(struct dentry *dentry) { }
@@ -588,7 +589,11 @@ static inline void ovl_snapshot_drop_write(struct dentry *dentry) { }
 
 static inline int ovl_snapshot_fs_register(void) { return 0; }
 static inline void ovl_snapshot_fs_unregister(void) { }
-static inline int ovl_snapshot_want_write(struct dentry *dentry) { return 0; }
+static inline int ovl_snapshot_want_write(struct dentry *dentry,
+					  bool new_is_dir)
+{
+	return 0;
+}
 static inline void ovl_snapshot_drop_write(struct dentry *dentry) { }
 
 static inline bool ovl_is_snapshot_fs_type(struct super_block *sb)
