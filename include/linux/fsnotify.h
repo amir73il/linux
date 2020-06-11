3fbbed49414900978adfd2ba1bb58e75478bdea7
Amir Goldstein <amir73il@gmail.com>
2020-06-11 14:30:00 +0300

    fsnotify: remove check that source dentry is positive
    Remove the unneeded check for positive source dentry in
    fsnotify_move().
    
    fsnotify_move() hook is mostly called from vfs_rename() under
    lock_rename() and vfs_rename() starts with may_delete() test that
    verifies positive source dentry.  The only other caller of
    fsnotify_move() - debugfs_rename() also verifies positive source.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- include/linux/fsnotify.h
+++ include/linux/fsnotify.h
@@ -149,8 +149,7 @@ static inline void fsnotify_move(struct inode *old_dir, struct inode *new_dir,
 	if (target)
 		fsnotify_link_count(target);
 
-	if (source)
-		fsnotify(source, mask, source, FSNOTIFY_EVENT_INODE, NULL, 0);
+	fsnotify(source, mask, source, FSNOTIFY_EVENT_INODE, NULL, 0);
 	audit_inode_child(new_dir, moved, AUDIT_TYPE_CHILD_CREATE);
 }
 
