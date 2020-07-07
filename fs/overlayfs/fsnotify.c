bf8f45c09de6141249c5ccd4c23cd6c027b0d80d
Amir Goldstein <amir73il@gmail.com>
2020-07-07 15:19:41 +0300

    ovl: implement lookup in change tracking snapshot
    Snapshot lookup is about finding out if a lower path has already been
    copied up or whited out in the change tracking snapshot.  This is done
    in a similar way of decoding a lower nfs file handle of an overlay
    mount without directory inode index entires and redirects.
    
    Before modification of lower layer path, we look for a dentry in the
    overlay snapshot whose lower is the underlying lower path.
    
    When looking up in overlay snapshot, what we need to know about the
    overlay snapshot dentry is if it is positive/upper/opaque so we know if
    it the lower object was already copied up or whited out in snapshot.
    
    We rely on snapshot lookup to populate the dentry and inode cache of the
    overlay filesystem, so we can quickly find the overlay snapshot dentry
    by the underlying dentry on subsequent calls.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/fsnotify.c
+++ fs/overlayfs/fsnotify.c
@@ -9,7 +9,76 @@
 
 #include <linux/fs.h>
 #include <linux/fsnotify.h>
+#include <linux/namei.h>
 #include "overlayfs.h"
+
+/*
+ * Check if lower dir or its child need to be copied to snapshot.
+ *
+ * We lookup directory in overlay snapshot whose lower is @lowerdir and
+ * we lookup non-directory and negative dentries by @name relative to
+ * snapshot's parent directory.
+ *
+ * Returns error if failed to lookup overlay snapshot dentry.
+ * Returns NULL if found an opaque or pure upper snapshot dentry.
+ * Returns the found overlay snapshot dentry otherwise.
+ */
+static struct dentry *ovl_snapshot_lookup(struct super_block *sb,
+					  struct dentry *lowerdir,
+					  const struct qstr *name)
+{
+	const struct cred *old_cred = ovl_override_creds(sb);
+	struct dentry *snapdir, *snap = NULL;
+	int err;
+
+	/* Find directory whose lower is @lowerdir in overlay snapshot mount */
+	snapdir = ovl_lookup_real(sb, lowerdir, &OVL_FS(sb)->layers[1]);
+	if (IS_ERR(snapdir)) {
+		err = PTR_ERR(snapdir);
+		snap = snapdir;
+		snapdir = NULL;
+		goto out;
+	}
+
+	/*
+	 * Negative dentries and non-directory dentries cannot be found by lower
+	 * inode, so we need to look them up by name after looking up parent by
+	 * @lowerdir.
+	 */
+	if (name) {
+		snap = lookup_one_len_unlocked(name->name, snapdir, name->len);
+		if (IS_ERR(snap))
+			goto out;
+	} else {
+		snap = dget(snapdir);
+	}
+
+	/* Whiteout/opaque in snapshot returns NULL for noop */
+	if (ovl_dentry_is_opaque(snap))
+		goto noop;
+
+	/*
+	 * Return negative snapshot dentry to be whited out in snapshot
+	 * and non-upper snapshot dentry to be copied up.
+	 */
+	if (d_is_negative(snap) || !ovl_dentry_has_upper_alias(snap))
+		goto out;
+
+	/* Pure upper in snapshot returns NULL for noop */
+	if (!d_is_dir(snap) || !ovl_dentry_lower(snap))
+		goto noop;
+
+out:
+	dput(snapdir);
+	revert_creds(old_cred);
+
+	return snap;
+
+noop:
+	dput(snap);
+	snap = NULL;
+	goto out;
+}
 
 #define OVL_FSNOTIFY_MASK (FS_PRE_MODIFY | FS_PRE_MODIFY_NAME)
 
@@ -27,13 +96,16 @@ static int ovl_handle_event(struct fsnotify_group *group, u32 mask,
 			    struct fsnotify_iter_info *iter_info)
 {
 	struct super_block *sb = group->private;
+	const struct path *lowerpath = fsnotify_data_path(data, data_type);
+	struct dentry *lowerdir = NULL;
+	int err = 0;
 
 	/* Should be no events from inode/mount marks */
 	if (WARN_ON_ONCE(fsnotify_iter_inode_mark(iter_info)) ||
 	    WARN_ON_ONCE(fsnotify_iter_vfsmount_mark(iter_info)))
 		return 0;
 
-	if (WARN_ON_ONCE(!(mask & OVL_FSNOTIFY_MASK)))
+	if (WARN_ON(!(mask & OVL_FSNOTIFY_MASK)) || !lowerpath)
 		return 0;
 
 	/* Don't handle events before overlay mount is done */
@@ -41,7 +113,59 @@ static int ovl_handle_event(struct fsnotify_group *group, u32 mask,
 		return 0;
 	smp_rmb();
 
-	return 0;
+	if (mask & FS_PRE_MODIFY_NAME) {
+		/*
+		 * We get here from a call to either mnt_want_write_name() or
+		 * mnt_want_write_rename() (i.e. link or unlink an entry) -
+		 * record change in dir/name.
+		 */
+		lowerdir = dget(lowerpath->dentry);
+		WARN_ON_ONCE(d_inode(lowerdir) != dir);
+		WARN_ON_ONCE(!name);
+
+		pr_debug("%s: %pd2/%s mask=%x\n", __func__, lowerdir, name->name, mask);
+	} else if (mask & FS_PRE_MODIFY) {
+		/*
+		 * Event is on dir itself or on a non-dir child -
+		 * record change in dir (without name).
+		 *
+		 * We get here from a call to either file_start_write() or
+		 * mnt_want_write_path().
+		 *
+		 * If event in on a non-dir child, we need to get the unstable
+		 * parent because event data is the path to the non-dir child,
+		 * not to the parent.
+		 *
+		 * If a rename came in between the pre-modify event and now and
+		 * moved child away from its parent, the rename itself should
+		 * have generated pre-modify events that would have already
+		 * recorded the change in the old and new parents, so it does
+		 * not matter if we get the old or new parent.
+		 */
+		lowerdir = (mask & FS_ISDIR) ? dget(lowerpath->dentry) :
+					       dget_parent(lowerpath->dentry);
+		name = NULL;
+		/* Not interested in events from non-dir with no parent */
+		if (unlikely(!d_is_dir(lowerdir)))
+			goto out;
+
+		pr_debug("%s: %pd2 mask=%x\n", __func__, lowerdir, mask);
+	}
+
+	if (lowerdir) {
+		struct dentry *snap;
+
+		/* TODO: copy to snapshot */
+		snap = ovl_snapshot_lookup(sb, lowerdir, name);
+		if (!IS_ERR(snap))
+			dput(snap);
+		err = -EPERM;
+	}
+
+out:
+	dput(lowerdir);
+
+	return err;
 }
 
 static int ovl_add_sb_mark(struct fsnotify_group *group,
