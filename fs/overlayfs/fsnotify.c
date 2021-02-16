e66bedbcb485737bf24aae85ab16af2556604a4e
Amir Goldstein <amir73il@gmail.com>
2021-02-16 14:31:38 +0200

    ovl: implement lookup in change tracking snapshot index
    Snapshot index lookup is about finding out if a lower directory has
    already been recorded in the change tracking snapshot.  This is done
    in a similar way of decoding a lower nfs file handle of an overlay
    mount.
    
    Before modification of lower layer path, we look for an index entry for
    the lower dir in index dir.
    
    When looking up in overlay snapshot index, what we need to know about the
    overlay index entry is if it is positive/negative/whiteout so we know if
    the lower object change has already been recorded.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/fsnotify.c
+++ fs/overlayfs/fsnotify.c
@@ -9,6 +9,8 @@
 
 #include <linux/fs.h>
 #include <linux/fsnotify.h>
+#include <linux/namei.h>
+#include <linux/ratelimit.h>
 #include "../mount.h"
 #include "overlayfs.h"
 
@@ -20,6 +22,100 @@ struct ovl_mark {
 	struct fsnotify_mark fsn_mark;
 	/* TBD */
 };
+
+static struct dentry *ovl_lookup_lower_unlocked(struct super_block *sb,
+						struct dentry *lowerdir,
+						const struct qstr *name)
+{
+	const struct cred *old_cred = ovl_override_creds(sb);
+	struct dentry *dentry;
+
+	dentry = lookup_one_len_unlocked(name->name, lowerdir, name->len);
+	revert_creds(old_cred);
+
+	return dentry;
+}
+
+/*
+ * Check if lower dir needs to be indexed.
+ *
+ * We lookup the @lowerdir index entry.
+ * If found negative, we may want to make it positive.
+ * If found postive, we may want to make it a whiteout.
+ * If found whiteout, we have nothing more to do.
+ *
+ * Returns the positive or negative index entry.
+ * Returns NULL if no need to index index lowerdir.
+ * Returns error if failed to lookup index entry.
+ */
+static struct dentry *ovl_lookup_lowerdir_index(struct super_block *sb,
+						struct dentry *lowerdir)
+{
+	struct ovl_fs *ofs = sb->s_fs_info;
+	struct dentry *index;
+	struct qstr name = {};
+	int err;
+
+	err = ovl_get_index_name(ofs, lowerdir, &name);
+	if (err)
+		return ERR_PTR(err);
+
+	index = ovl_lookup_lower_unlocked(sb, ofs->indexdir, &name);
+	if (IS_ERR(index)) {
+		err = PTR_ERR(index);
+		goto fail;
+	}
+
+	if (d_is_dir(index) || d_is_negative(index)) {
+		/* Return found positive/negative index entry for hit/miss */
+	} else if (ovl_is_whiteout(index)) {
+		/* Whiteout index returns NULL for noop */
+		dput(index);
+		index = NULL;
+	} else {
+		dput(index);
+		err = -ENOTDIR;
+		goto fail;
+	}
+
+	pr_debug("%s: %pd2 is %s\n", __func__, lowerdir,
+		 !index ? "moved" : d_is_dir(index) ? "indexed" : "unchanged");
+	kfree(name.name);
+
+	return index;
+
+fail:
+	pr_warn_ratelimited("failed lowerdir index lookup (ino=%lu, key=%.*s, err=%i)\n",
+			    d_inode(lowerdir)->i_ino, name.len, name.name, err);
+	kfree(name.name);
+
+	return ERR_PTR(err);
+}
+
+/*
+ * Record change in index if needed before lower modification.
+ *
+ * Called without any filesystem locks held.
+ */
+static int ovl_handle_want_write(struct super_block *sb, u32 mask,
+				 struct dentry *lowerdir)
+{
+	struct dentry *index;
+	int err = 0;
+
+	index = ovl_lookup_lowerdir_index(sb, lowerdir);
+	if (IS_ERR_OR_NULL(index)) {
+		err = PTR_ERR(index);
+		index = NULL;
+	} else if (d_is_negative(index)) {
+		/* Deny modification unless change is recorded in index */
+		err = -ENOENT;
+	}
+
+	dput(index);
+
+	return err;
+}
 
 static void ovl_add_ignored_mark(struct fsnotify_group *group,
 				 struct dentry *lowerdir, u32 mask)
@@ -99,6 +195,14 @@ static int ovl_handle_event(struct fsnotify_group *group, u32 mask,
 	if (!ovl_indexdir(sb)) {
 		ret = -EROFS;
 		goto out;
+	}
+
+	ret = ovl_handle_want_write(sb, mask, lowerdir);
+	if (ret < 0)
+		goto out;
+
+	if (mask & FS_MOVE_INTENT) {
+		/* TODO: whiteout index before move of lower */
 	}
 
 out:
