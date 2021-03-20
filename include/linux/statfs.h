98720a9a3112cbf884efb07bfa612f5d910dff0a
Amir Goldstein <amir73il@gmail.com>
2021-03-20 11:59:08 +0200

    fs: introduce a wrapper uuid_to_fsid()
    Some filesystem's use a digest of their uuid for f_fsid.
    Create a simple wrapper for this open coded folding.
    
    Filesystems that have a non null uuid but use the block device
    number for f_fsid may also consider using this helper.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- include/linux/statfs.h
+++ include/linux/statfs.h
@@ -50,4 +50,11 @@ static inline __kernel_fsid_t u64_to_fsid(u64 v)
 	return (__kernel_fsid_t){.val = {(u32)v, (u32)(v>>32)}};
 }
 
+/* Fold 16 bytes uuid to 64 bit fsid */
+static inline __kernel_fsid_t uuid_to_fsid(__u8 *uuid)
+{
+	return u64_to_fsid(le64_to_cpup((void *)uuid) ^
+		le64_to_cpup((void *)(uuid + sizeof(u64))));
+}
+
 #endif
