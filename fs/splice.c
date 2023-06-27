f0d04b6c18d8b28e8d51e20f7541f726822cee5c
Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>
2023-06-27 22:51:04 +0200

    splice: fsnotify_access(in), fsnotify_modify(out) on success in tee
    Same logic applies here: this can fill up the pipe, and pollers that rely
    on getting IN_MODIFY notifications never wake up.
    
    Fixes: 983652c69199 ("splice: report related fsnotify events")
    Link: https://lore.kernel.org/linux-fsdevel/jbyihkyk5dtaohdwjyivambb2gffyjs3dodpofafnkkunxq7bu@jngkdxx65pux/t/#u
    Link: https://bugs.debian.org/1039488
    Signed-off-by: Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>
    Reviewed-by: Amir Goldstein <amir73il@gmail.com>

--- fs/splice.c
+++ fs/splice.c
@@ -1802,6 +1802,11 @@ long do_tee(struct file *in, struct file *out, size_t len, unsigned int flags)
 		}
 	}
 
+	if (ret > 0) {
+		fsnotify_access(in);
+		fsnotify_modify(out);
+	}
+
 	return ret;
 }
 
