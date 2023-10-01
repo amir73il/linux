5f98012eaabf93d9096b0fada1b9e1f1e937a9d6
Amir Goldstein <amir73il@gmail.com>
2023-10-01 21:35:25 +0300

    ovl: factor out some common helpers for backing files io
    Overlayfs submits files io to backing files on other filesystems.
    
    Factor out some common helpers to perform io to backing files, into
    fs/backing-file.c.
    
    Suggested-by: Miklos Szeredi <miklos@szeredi.hu>
    Link: https://lore.kernel.org/r/CAJfpeguhmZbjP3JLqtUy0AdWaHOkAPWeP827BBWwRFEAUgnUcQ@mail.gmail.com
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- include/linux/backing-file.h
+++ include/linux/backing-file.h
@@ -9,9 +9,17 @@
 #define _LINUX_BACKING_FILE_H
 
 #include <linux/file.h>
+#include <linux/uio.h>
+#include <linux/fs.h>
 
 struct file *backing_file_open(const struct path *user_path, int flags,
 			       const struct path *real_path,
 			       const struct cred *cred);
+ssize_t backing_file_read_iter(struct file *file, struct iov_iter *iter,
+			       struct kiocb *iocb, int flags,
+			       void (*accessed)(struct file *));
+ssize_t backing_file_write_iter(struct file *file, struct iov_iter *iter,
+				struct kiocb *iocb, int flags,
+				void (*modified)(struct file *));
 
 #endif /* _LINUX_BACKING_FILE_H */
