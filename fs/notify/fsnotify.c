amir73il/linux

commit a5e57b4d370c6d320e5bfb0c919fe00aee29e039

fsnotify: optimize the case of no permission event watchers
Commit e43de7f0862b ("fsnotify: optimize the case of no marks of any type")
optimized the case where there are no fsnotify watchers on any of the
filesystem's objects.

It is quite common for a system to have a single local filesystem and
it is quite common for the system to have some inotify watches on some
config files or directories, so the optimization of no marks at all is
often not in effect.

Permission event watchers, which require high priority group are more
rare, so optimizing the case of no marks og high priority groups can
improve performance for more systems, especially for performance
sensitive io workloads.

Count per-sb watched objects by high priority groups and use that the
optimize out the call to __fsnotify_parent() and fsnotify() in fsnotify
permission hooks.

Signed-off-by: Amir Goldstein <amir73il@gmail.com>
Signed-off-by: Jan Kara <jack@suse.cz>
Message-Id: <20240317184154.1200192-11-amir73il@gmail.com>

fs/notify/fsnotify.c
@@ -100,6 +100,9 @@ void fsnotify_sb_delete(struct super_block *sb)
 	/* Wait for outstanding object references from connectors */
 	wait_var_event(fsnotify_sb_watched_objects(sb),
 		       !atomic_long_read(fsnotify_sb_watched_objects(sb)));
+	WARN_ON(fsnotify_sb_has_priority_watchers(sb, FSNOTIFY_PRIO_CONTENT));
+	WARN_ON(fsnotify_sb_has_priority_watchers(sb,
+						  FSNOTIFY_PRIO_PRE_CONTENT));
 	kfree(sbinfo);
 }
 

include/linux/fsnotify.h
@@ -17,8 +17,9 @@
 #include <linux/slab.h>
 #include <linux/bug.h>
 
-/* Are there any inode/mount/sb objects that are being watched at all? */
-static inline bool fsnotify_sb_has_watchers(struct super_block *sb)
+/* Are there any inode/mount/sb objects watched with priority prio or above? */
+static inline bool fsnotify_sb_has_priority_watchers(struct super_block *sb,
+						     int prio)
 {
 	struct fsnotify_sb_info *sbinfo = fsnotify_sb_info(sb);
 
@@ -26,7 +27,13 @@ static inline bool fsnotify_sb_has_watchers(struct super_block *sb)
 	if (!sbinfo)
 		return false;
 
-	return atomic_long_read(&sbinfo->watched_objects);
+	return atomic_long_read(&sbinfo->watched_objects[prio]);
+}
+
+/* Are there any inode/mount/sb objects that are being watched at all? */
+static inline bool fsnotify_sb_has_watchers(struct super_block *sb)
+{
+	return fsnotify_sb_has_priority_watchers(sb, 0);
 }
 
 /*
@@ -109,6 +116,12 @@ static inline int fsnotify_file(struct file *file, __u32 mask)
 		return 0;
 
 	path = &file->f_path;
+	/* Permission events require group prio >= FSNOTIFY_PRIO_CONTENT */
+	if (mask & ALL_FSNOTIFY_PERM_EVENTS &&
+	    !fsnotify_sb_has_priority_watchers(path->dentry->d_sb,
+					       FSNOTIFY_PRIO_CONTENT))
+		return 0;
+
 	return fsnotify_parent(path->dentry, mask, path, FSNOTIFY_EVENT_PATH);
 }
 

fs/notify/mark.c
@@ -161,13 +161,36 @@ static void fsnotify_put_inode_ref(struct inode *inode)
 static void fsnotify_update_sb_watchers(struct super_block *sb,
 					struct fsnotify_mark_connector *conn)
 {
+	struct fsnotify_sb_info *sbinfo = fsnotify_sb_info(sb);
 	bool is_watched = conn->flags & FSNOTIFY_CONN_FLAG_IS_WATCHED;
-	bool has_marks = conn->obj && !hlist_empty(&conn->list);
+	struct fsnotify_mark *first_mark = NULL;
+	unsigned int highest_prio = 0;
 
-	if (has_marks && !is_watched) {
+	if (conn->obj)
+		first_mark = hlist_entry_safe(conn->list.first,
+					      struct fsnotify_mark, obj_list);
+	if (first_mark)
+		highest_prio = first_mark->group->priority;
+	if (WARN_ON(highest_prio >= __FSNOTIFY_PRIO_NUM))
+		highest_prio = 0;
+
+	/*
+	 * If the highest priority of group watching this object is prio,
+	 * then watched object has a reference on counters [0..prio].
+	 * Update priority >= 1 watched objects counters.
+	 */
+	for (unsigned int p = conn->prio + 1; p <= highest_prio; p++)
+		atomic_long_inc(&sbinfo->watched_objects[p]);
+	for (unsigned int p = conn->prio; p > highest_prio; p--)
+		atomic_long_dec(&sbinfo->watched_objects[p]);
+	conn->prio = highest_prio;
+
+	/* Update priority >= 0 (a.k.a total) watched objects counter */
+	BUILD_BUG_ON(FSNOTIFY_PRIO_NORMAL != 0);
+	if (first_mark && !is_watched) {
 		conn->flags |= FSNOTIFY_CONN_FLAG_IS_WATCHED;
 		fsnotify_get_sb_watched_objects(sb);
-	} else if (!has_marks && is_watched) {
+	} else if (!first_mark && is_watched) {
 		conn->flags &= ~FSNOTIFY_CONN_FLAG_IS_WATCHED;
 		fsnotify_put_sb_watched_objects(sb);
 	}
@@ -600,6 +623,7 @@ static int fsnotify_attach_connector_to_object(fsnotify_connp_t *connp,
 	spin_lock_init(&conn->lock);
 	INIT_HLIST_HEAD(&conn->list);
 	conn->flags = 0;
+	conn->prio = 0;
 	conn->type = obj_type;
 	conn->obj = obj;
 

include/linux/fsnotify_backend.h
@@ -468,7 +468,8 @@ FSNOTIFY_ITER_FUNCS(sb, SB)
  */
 struct fsnotify_mark_connector {
 	spinlock_t lock;
-	unsigned short type;	/* Type of object [lock] */
+	unsigned char type;	/* Type of object [lock] */
+	unsigned char prio;	/* Highest priority group */
 #define FSNOTIFY_CONN_FLAG_IS_WATCHED	0x01
 #define FSNOTIFY_CONN_FLAG_HAS_IREF	0x02
 	unsigned short flags;	/* flags [lock] */
@@ -490,8 +491,12 @@ struct fsnotify_sb_info {
 	/*
 	 * Number of inode/mount/sb objects that are being watched in this sb.
 	 * Note that inodes objects are currently double-accounted.
+	 *
+	 * The value in watched_objects[prio] is the number of objects that are
+	 * watched by groups of priority >= prio, so watched_objects[0] is the
+	 * total number of watched objects in this sb.
 	 */
-	atomic_long_t watched_objects;
+	atomic_long_t watched_objects[__FSNOTIFY_PRIO_NUM];
 };
 
 static inline struct fsnotify_sb_info *fsnotify_sb_info(struct super_block *sb)
@@ -505,7 +510,7 @@ static inline struct fsnotify_sb_info *fsnotify_sb_info(struct super_block *sb)
 
 static inline atomic_long_t *fsnotify_sb_watched_objects(struct super_block *sb)
 {
-	return &fsnotify_sb_info(sb)->watched_objects;
+	return &fsnotify_sb_info(sb)->watched_objects[0];
 }
 
 /*

commit 28d5b4a88241d36788173a412113e2c55a7fd4e1

fsnotify: avoid multiple fsnotify_sb_info() access in permission hooks
fsnotify_file() hooks call fsnotify_sb_info() accessor twice and
fsnotify permission hooks call it even more times.

Call the accessor only once in all fsnotify hooks to allow for better
compiler optimizations.

Fixes: a5e57b4d370c ("fsnotify: optimize the case of no permission event watchers")
Reported-by: kernel test robot <oliver.sang@intel.com>
Link: https://lore.kernel.org/oe-lkp/202404101624.85684be8-oliver.sang@intel.com
Link: https://lore.kernel.org/oe-lkp/202405291640.2016ebfe-oliver.sang@intel.com
Signed-off-by: Amir Goldstein <amir73il@gmail.com>

fs/notify/fsnotify.c
@@ -100,9 +100,8 @@ void fsnotify_sb_delete(struct super_block *sb)
 	/* Wait for outstanding object references from connectors */
 	wait_var_event(fsnotify_sb_watched_objects(sb),
 		       !atomic_long_read(fsnotify_sb_watched_objects(sb)));
-	WARN_ON(fsnotify_sb_has_priority_watchers(sb, FSNOTIFY_PRIO_CONTENT));
-	WARN_ON(fsnotify_sb_has_priority_watchers(sb,
-						  FSNOTIFY_PRIO_PRE_CONTENT));
+	WARN_ON(fsnotify_sbinfo_has_watchers(sbinfo, FSNOTIFY_PRIO_CONTENT));
+	WARN_ON(fsnotify_sbinfo_has_watchers(sbinfo, FSNOTIFY_PRIO_PRE_CONTENT));
 }
 
 void fsnotify_sb_free(struct super_block *sb)

include/linux/fsnotify.h
@@ -18,11 +18,9 @@
 #include <linux/bug.h>
 
 /* Are there any inode/mount/sb objects watched with priority prio or above? */
-static inline bool fsnotify_sb_has_priority_watchers(struct super_block *sb,
-						     int prio)
+static inline bool fsnotify_sbinfo_has_watchers(struct fsnotify_sb_info *sbinfo,
+						int prio)
 {
-	struct fsnotify_sb_info *sbinfo = fsnotify_sb_info(sb);
-
 	/* Were any marks ever added to any object on this sb? */
 	if (!sbinfo)
 		return false;
@@ -33,7 +31,9 @@ static inline bool fsnotify_sb_has_priority_watchers(struct super_block *sb,
 /* Are there any inode/mount/sb objects that are being watched at all? */
 static inline bool fsnotify_sb_has_watchers(struct super_block *sb)
 {
-	return fsnotify_sb_has_priority_watchers(sb, 0);
+	struct fsnotify_sb_info *sbinfo = fsnotify_sb_info(sb);
+
+	return fsnotify_sbinfo_has_watchers(sbinfo, 0);
 }
 
 /*
@@ -73,12 +73,13 @@ static inline void fsnotify_inode(struct inode *inode, __u32 mask)
 }
 
 /* Notify this dentry's parent about a child's events. */
-static inline int fsnotify_parent(struct dentry *dentry, __u32 mask,
+static inline int fsnotify_parent(struct fsnotify_sb_info *sbinfo,
+				  struct dentry *dentry, __u32 mask,
 				  const void *data, int data_type)
 {
 	struct inode *inode = d_inode(dentry);
 
-	if (!fsnotify_sb_has_watchers(inode->i_sb))
+	if (!fsnotify_sbinfo_has_watchers(sbinfo, 0))
 		return 0;
 
 	if (S_ISDIR(inode->i_mode)) {
@@ -105,33 +106,46 @@ notify_child:
  */
 static inline void fsnotify_dentry(struct dentry *dentry, __u32 mask)
 {
-	fsnotify_parent(dentry, mask, dentry, FSNOTIFY_EVENT_DENTRY);
+	struct fsnotify_sb_info *sbinfo = fsnotify_sb_info(dentry->d_sb);
+
+	fsnotify_parent(sbinfo, dentry, mask, dentry, FSNOTIFY_EVENT_DENTRY);
 }
 
-static inline int fsnotify_file(struct file *file, __u32 mask)
+static inline struct fsnotify_sb_info *fsnotify_file_sbinfo(struct file *f)
 {
-	const struct path *path;
+	if (f->f_mode & FMODE_NONOTIFY)
+		return NULL;
 
-	if (file->f_mode & FMODE_NONOTIFY)
-		return 0;
+	return fsnotify_sb_info(f->f_path.dentry->d_sb);
+}
 
-	path = &file->f_path;
-	/* Permission events require group prio >= FSNOTIFY_PRIO_CONTENT */
-	if (mask & ALL_FSNOTIFY_PERM_EVENTS &&
-	    !fsnotify_sb_has_priority_watchers(path->dentry->d_sb,
-					       FSNOTIFY_PRIO_CONTENT))
-		return 0;
+static inline int fsnotify_file(struct file *f, __u32 mask)
+{
+	struct fsnotify_sb_info *sbinfo = fsnotify_file_sbinfo(f);
 
-	return fsnotify_parent(path->dentry, mask, path, FSNOTIFY_EVENT_PATH);
+	return fsnotify_parent(sbinfo, f->f_path.dentry, mask, &f->f_path,
+			       FSNOTIFY_EVENT_PATH);
 }
 
 #ifdef CONFIG_FANOTIFY_ACCESS_PERMISSIONS
+static inline int fsnotify_perm(struct fsnotify_sb_info *sbinfo,
+				struct file *f, __u32 mask)
+{
+	/* Permission events require group prio >= FSNOTIFY_PRIO_CONTENT */
+	if (!fsnotify_sbinfo_has_watchers(sbinfo, FSNOTIFY_PRIO_CONTENT))
+		return 0;
+
+	return fsnotify_parent(sbinfo, f->f_path.dentry, mask, &f->f_path,
+			       FSNOTIFY_EVENT_PATH);
+}
+
 /*
  * fsnotify_file_area_perm - permission hook before access to file range
  */
 static inline int fsnotify_file_area_perm(struct file *file, int perm_mask,
 					  const loff_t *ppos, size_t count)
 {
+	struct fsnotify_sb_info *sbinfo = fsnotify_file_sbinfo(file);
 	__u32 fsnotify_mask = FS_ACCESS_PERM;
 
 	/*
@@ -144,7 +158,7 @@ static inline int fsnotify_file_area_perm(struct file *file, int perm_mask,
 	if (!(perm_mask & MAY_READ))
 		return 0;
 
-	return fsnotify_file(file, fsnotify_mask);
+	return fsnotify_perm(sbinfo, file, fsnotify_mask);
 }
 
 /*
@@ -160,15 +174,16 @@ static inline int fsnotify_file_perm(struct file *file, int perm_mask)
  */
 static inline int fsnotify_open_perm(struct file *file)
 {
+	struct fsnotify_sb_info *sbinfo = fsnotify_file_sbinfo(file);
 	int ret;
 
 	if (file->f_flags & __FMODE_EXEC) {
-		ret = fsnotify_file(file, FS_OPEN_EXEC_PERM);
+		ret = fsnotify_perm(sbinfo, file, FS_OPEN_EXEC_PERM);
 		if (ret)
 			return ret;
 	}
 
-	return fsnotify_file(file, FS_OPEN_PERM);
+	return fsnotify_perm(sbinfo, file, FS_OPEN_PERM);
 }
 
 #else