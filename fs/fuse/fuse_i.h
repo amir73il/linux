094631d12a3231f910a081c1e6d0fa2dd63098a4
Amir Goldstein <amir73il@gmail.com>
2025-07-14 16:40:23 +0200

    fuse: implement passthrough for getattr/statx
    Call vfs_getattr() on backing inode to respond to a user statx(2)
    request and update the fuse inode attributes with the response.
    
    For now, we assume that calling vfs_getattr() on backing inode is cheap,
    so we never use cached attributed and always update fuse inode attributes
    from backing attributes in fuse_permission() and fuse_update_attributes().
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -1207,6 +1207,9 @@ void fuse_init_symlink(struct inode *inode);
 /**
  * Change attributes of an inode
  */
+void fuse_kstat_to_attr(struct mnt_idmap *idmap, struct inode *inode,
+			const struct kstat *stat, struct fuse_attr *attr,
+			struct fuse_statx *sx);
 void fuse_change_attributes(struct inode *inode, struct fuse_attr *attr,
 			    struct fuse_statx *sx,
 			    u64 attr_valid, u64 attr_version);
@@ -1526,7 +1529,8 @@ void fuse_file_release(struct inode *inode, struct fuse_file *ff,
 	(FUSE_PASSTHROUGH_RW_OPS | FUSE_PASSTHROUGH_OP_READDIR)
 
 /* Inode passthrough operations for backing file attached to inode */
-#define FUSE_PASSTHROUGH_INODE_OPS (0)
+#define FUSE_PASSTHROUGH_INODE_OPS \
+	(FUSE_PASSTHROUGH_OP_GETATTR | FUSE_PASSTHROUGH_OP_STATX)
 
 #define FUSE_BACKING_MAP_OP(map, op) \
 	((map)->ops_mask & FUSE_PASSTHROUGH_OP(op))
@@ -1615,6 +1619,10 @@ static inline bool fuse_inode_passthrough_op(struct inode *inode,
 
 	return fb && fb->ops_mask & FUSE_PASSTHROUGH_OP(op);
 }
+
+int fuse_passthrough_getattr(struct mnt_idmap *idmap, struct inode *inode,
+			     struct kstat *stat, u32 request_mask,
+			     unsigned int flags);
 
 #ifdef CONFIG_SYSCTL
 extern int fuse_sysctl_register(void);
